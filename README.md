b83519a2382b04e49cfe8826fddcd3d45979e3f8
Marvin Menzerath <marvin.menzerath@gmx.de>
2014-08-13 18:29:02 +0200

    Added Source-Code

---
+++ README.md
@@ -0,0 +1,48 @@
+# SimpleTabColors
+This tiny Plugin modifies the Tab-List to show colored Player-Names. To give a Player a certain color give him the specific Permission.
+
+## Install
+Grab a current Release from [here](https://github.com/MarvinMenzerath/SimpleTabColors/releases) and copy it into your Plugins-Directory.  
+Now you are able to give the Player/Group a Permission (`simpletabcolors.X`) which will result in a specific color.
+
+### Example Configuration
+```
+groups:
+  User:
+    default: true
+    build: false
+    prefix: '&7[User] '
+    permissions:
+    - simpletabcolors.7
+    - sg.arena.join.*
+    - sg.lobby.join
+    - sg.arena.vote
+  Premium:
+    default: false
+    build: false
+    prefix: '&6[Premium] '
+    permissions:
+    - simpletabcolors.6
+    - sg.arena.join.*
+    - sg.lobby.join
+    - sg.arena.vote
+    - sg.arena.spectate
+    - sg.arena.forcestart
+  Admin:
+    default: false
+    build: false
+    prefix: '&c[Admin] '
+    permissions:
+    - simpletabcolors.c
+    - -simpletabcolors.*
+    - '*'
+users:
+  ...
+```
+
+## License
+Copyright (C) 2014 [Marvin Menzerath](http://menzerath.eu)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the [GNU General Public License](https://github.com/MarvinMenzerath/TabListPerWorld/blob/master/LICENSE) for more details.
---
+++ src/main/java/eu/menzerath/bukkit/SimpleTabColors.java
@@ -0,0 +1,57 @@
+package eu.menzerath.bukkit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleTabColors extends JavaPlugin implements Listener {
+
+    /**
+     * Register the 'PlayerJoinEvent' and start a repeating Task, which updates the colored names every second (20 Ticks)
+     */
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			public void run() {
+				for (Player player : getServer().getOnlinePlayers())
+					updateName(player);
+			}
+		}, 0L, 20L);
+	}
+
+    /**
+     * Run the 'updateName(Player player)' method whenever a new player joins the server after a small delay of one tick
+     */
+	@EventHandler
+	public void onPlayerJoin(final PlayerJoinEvent event) {
+		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			public void run() {
+				updateName(event.getPlayer());
+			}
+		}, 1);
+	}
+
+    /**
+     * Colors the name of the specified Player in the TabList according to the Player's permission
+     * Also shorts the 'DisplayName' to fit with the two-digit ColorCode
+     */
+	public void updateName(Player player) {
+		String name = player.getDisplayName();
+		for (ChatColor color : ChatColor.values()) {
+			String permission = "simpletabcolors." + color.getChar();
+			if (player.hasPermission(permission)) {
+				if (name.length() > 14) {
+					name = color + name.substring(0, 14);
+				} else {
+					name = color + name;
+				}
+				player.setPlayerListName(name);
+				return;
+			}
+		}
+	}
+}
