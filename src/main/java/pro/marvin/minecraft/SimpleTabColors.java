7dbb31285be4d6e0688ae264105acb5da7d648ce
Marvin Menzerath <github@marvin-menzerath.de>
2017-01-07 20:46:49 +0100

    Updated dependencies and moved to Java 8

--- src/main/java/pro/marvin/minecraft/SimpleTabColors.java
+++ src/main/java/pro/marvin/minecraft/SimpleTabColors.java
@@ -1,4 +1,4 @@
-package eu.menzerath.bukkit;
+package pro.marvin.minecraft;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -9,36 +9,30 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleTabColors extends JavaPlugin implements Listener {
 
-    /**
-     * Register the 'PlayerJoinEvent' and start a repeating Task, which updates the colored names every second (20 Ticks)
-     */
+	/**
+	 * Register the 'PlayerJoinEvent' and start a repeating Task, which updates the colored names every second (20 Ticks)
+	 */
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
-		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-			public void run() {
-				for (Player player : getServer().getOnlinePlayers())
-					updateName(player);
-			}
+		getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+			for (Player player : getServer().getOnlinePlayers())
+				updateName(player);
 		}, 0L, 20L);
 	}
 
-    /**
-     * Run the 'updateName(Player player)' method whenever a new player joins the server after a small delay of one tick
-     */
+	/**
+	 * Run the 'updateName(Player player)' method whenever a new player joins the server after a small delay of one tick
+	 */
 	@EventHandler
 	public void onPlayerJoin(final PlayerJoinEvent event) {
-		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			public void run() {
-				updateName(event.getPlayer());
-			}
-		}, 1);
+		getServer().getScheduler().scheduleSyncDelayedTask(this, () -> updateName(event.getPlayer()), 1);
 	}
 
-    /**
-     * Colors the name of the specified Player in the TabList according to the Player's permission
-     * Also shorts the 'DisplayName' to fit with the two-digit ColorCode
-     */
+	/**
+	 * Colors the name of the specified Player in the TabList according to the Player's permission
+	 * Also shorts the 'DisplayName' to fit with the two-digit ColorCode
+	 */
 	public void updateName(Player player) {
 		String name = player.getDisplayName();
 		for (ChatColor color : ChatColor.values()) {
